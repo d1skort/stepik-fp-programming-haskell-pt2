c7ae6f067d93ab275b28fe822ea40dfda6d8e505
Vladislav Pakhomov <d1skortneru@yandex.ru>
2018-12-07 13:15:57 +0300

    add functor for compose

---
+++ ApplicativeFunctor/1.5/cmpsFunctor.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE TypeOperators #-}
+module CmpsFunctor where
+
+
+newtype Cmps3 f g h a = Cmps3 { getCmps3 :: f (g (h a)) } deriving (Eq, Show)
+
+
+instance (Functor f, Functor g, Functor h) => Functor (Cmps3 f g h) where
+  -- fmap :: (a -> b) -> f a -> f b
+  -- fmap :: (a -> b) -> Cmps3 (f (g (h a))) -> Cmps3 (f (g (h b)))
+  fmap phi (Cmps3 x) = Cmps3 $ fmap (fmap (fmap phi)) x
