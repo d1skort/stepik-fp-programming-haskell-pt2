6feab5c649c5caebdf8aac36fb3d5ad5bcc12545
Vladislav Pakhomov <d1skortneru@yandex.ru>
2018-12-15 21:59:17 +0300

    add some foldable instances for tree (#1)
    * add some foldable instances for tree
    
    * fix pre order
    
    * add post order
    
    * add level order
    
    * refactor treeFoldable

---
+++ EffectControl/2.1/treeFoldable.hs
@@ -0,0 +1,41 @@
+module TreeFoldable where
+
+
+data Tree a = Nil | Branch (Tree a) a (Tree a) deriving (Eq, Show)
+newtype Preorder a = PreO (Tree a) deriving (Eq, Show)
+newtype Postorder a = PostO (Tree a) deriving (Eq, Show)
+newtype Levelorder a = LevelO (Tree a) deriving (Eq, Show)
+
+
+tree = Branch (Branch (Branch Nil 0 Nil) 2 (Branch Nil 3 Nil)) 6 (Branch (Branch Nil 7 Nil) 8 (Branch Nil 9 Nil))
+--       6
+--     /   \
+--    2     8
+--  /  \   / \
+-- 0   3  7   9
+
+instance Foldable Tree where
+  -- foldr :: (a -> b -> b) -> b -> t a -> b
+  foldr f ini Nil = ini
+  foldr f ini (Branch l x r) = foldr f (f x (foldr f ini r)) l
+
+
+instance Foldable Preorder where
+  -- foldr :: (a -> b -> b) -> b -> t a -> b
+  foldr f ini (PreO Nil) = ini
+  foldr f ini (PreO (Branch l x r)) = f x (foldr f (foldr f ini (PreO r)) (PreO l))
+
+
+instance Foldable Postorder where
+  -- foldr :: (a -> b -> b) -> b -> t a -> b
+  foldr f ini (PostO Nil) = ini
+  foldr f ini (PostO (Branch l x r)) = foldr f (foldr f (f x ini) (PostO r)) (PostO l)
+
+
+instance Foldable Levelorder where
+  -- foldr :: (a -> b -> b) -> b -> t a -> b
+  foldr f ini (LevelO Nil) = ini
+  foldr f ini (LevelO tree) = fun [tree] where
+    fun [] = ini
+    fun (Nil:xs) = fun xs
+    fun ((Branch l x r):xs) = f x $ fun (xs ++ [l, r])
