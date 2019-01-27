55c8edda80cd9c5a8987ff939b9283ffa492b669
Vladislav Pakhomov <d1skortneru@yandex.ru>
2019-01-27 17:00:07 +0300

    Checkpointed (#22)
    * figure out Checkpointed a signature
    
    * I see the future

---
+++ MonadsAndEffects/3.2/checkpointed.hs
@@ -0,0 +1,21 @@
+module Checkpointed where
+
+import Control.Monad.Cont
+
+
+type Checkpointed a = (a -> Cont a a) -> Cont a a
+
+
+addTens :: Int -> Checkpointed Int
+addTens x1 = \checkpoint -> do
+  checkpoint x1
+  let x2 = x1 + 10
+  checkpoint x2
+  let x3 = x2 + 10
+  checkpoint x3
+  let x4 = x3 + 10
+  return x4
+
+
+runCheckpointed :: (a -> Bool) -> Checkpointed a -> a
+runCheckpointed pr chkp = runCont (chkp (\x -> cont $ \c -> if (pr (c x)) then c x else x)) id
